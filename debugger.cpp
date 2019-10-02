df2e15fc024d97badcf04620b7607ef7f0a3cf15
zcysky <zcysky@126.com>
2019-10-02 21:44:28 +0800

    添加定位

---
+++ debugger.cpp
@@ -0,0 +1,14 @@
+//先写一个定位位置的，用于日后抛出报错
+//报错以后再写
+
+class Position{
+    public:
+    int line,col;
+    Position () {}
+    Position (int line,int col){
+        this->line=line;this->col=col;
+    }
+    friend void report(){
+        cerr<<"Line: "<<this->line<<",col: "<<this->col;
+    }
+};
