ca8a205065699e3f654fdccebd484c1f163df1a1
zcysky <zcysky@126.com>
2019-10-02 21:21:06 +0800

    完成header

--- header.cpp
+++ header.cpp
@@ -26,3 +26,34 @@ const int L_REQ=L_LEQ-1;
 const int L_NEQ=L_LEQ-1;
 const int L_AND=L_NEQ-1;
 const int L_OR=L_AND-1;
+
+//其他特殊标识
+const int ERROR=L_OR-1;
+const int EXIT=ERROR-1;
+const int COMMENT=EXIT-1;
+
+
+//定义运算符号
+//括号
+const int LC1="(";
+const int RC1=")";
+const int LC2="[";
+const int RC2="]";
+const int BEGIN="{";
+const int END="}";
+
+//双目基本运算
+const int PLUS="+";
+const int MINUS="-";
+const int MUL="*";
+const int DEV="/";
+const int MOD="%";
+
+//逻辑运算符
+const int NOT="!";
+const int ASSIGN="=";
+const int SEP=";";
+const int LESS="<";
+const int GREAT=">";
+
+#define dbg(x) cout<<#x<<" = "<<(x)<<endl;
