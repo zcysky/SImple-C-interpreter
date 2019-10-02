5b2911b0ffb5dc91485e7289519ac28d54c7dc0c
zcysky <zcysky@126.com>
2019-10-02 22:10:48 +0800

    更新token

--- lexer.cpp
+++ lexer.cpp
@@ -1,1 +1,34 @@
-#include
+#include "debugger.cpp"
+#include "header.cpp"
+
+class token{
+    public:
+    Position pos;//追踪位置，用于报错
+    int type,number;
+    string ident;
+
+    token(){}
+    token(Position pos,int type,const string &ident){
+        this->pos=pos;this->type=type;this->ident=s;
+    }
+    token(Position pos,int type){
+        this->pos=pos;this->type=type;
+    }
+    token(Position pos,int type,int number){
+        this->pos=pos;this->type=type;this->number=number;
+    }
+    inline void output(){
+        if(type=ERROR)cout<<"ERROR"<<endl;
+        if(type==IDENT)cout<<"Ident:"<<ident<<end;
+        if(type==NUMBER)cout<<"Number:"<<number<<endl;
+        if(type==KEYWORD)cout<<"Keyword:"<<ident<<endl;
+    }
+};
+
+
+class lexer{
+    map<string,int> table;
+    map<int,string> keys;
+    //上面的存储一个双射，分别表示标识符/关键字被映射的编号
+    
+};
