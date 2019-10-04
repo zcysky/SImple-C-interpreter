a7d4f78d2ec39623cd3d2a76d2b5ce701f231d1d
zcysky <zcysky@126.com>
2019-10-04 17:42:26 +0800

    完成var

--- parser.cpp
+++ parser.cpp
@@ -2,7 +2,7 @@
 #include "header.cpp"
 #include "lexer.cpp"
 
-class Tree;
+class AST;
 class VarDefiniton;
 class FuncDefinition;
 class Block;
@@ -20,7 +20,7 @@ class Exec;
 class visitor{
 
     public:
-    virtual void VisitTree(Tree *that){}
+    virtual void VisitTree(AST *that){}
     virtual void VisitVarDefinition(VarDefiniton *that){}
     virtual void VisitFuncDefinition(FuncDefinition *that){}
     virtual void VisitBlock(Block *that){}
@@ -42,9 +42,71 @@ class AST{
     public:
     AST(){}
     inline void setpos(const Position &pos){this->pos=pos;}
+    inline Position getpos(){return this->pos;}
     AST(const Position &pos){setpos(pos);}
     virtual ~AST(){}
-    
+    virtual void visit(visitor &vis){vis.VisitTree(this);}
+
 
 
 };
+
+class Var{
+
+    inline bool checkmul(int a,int b){return 1LL*a*b<=2147483647;}
+    inline int checkindex(const vector<int> &indexs){
+        int size=1;
+        for(int i=0;i<indexs.size();i++){
+            int v=indexs[i];
+            if(v<0||!checkmul(size,v)){/*报错：数组下标越界*/}
+            size*=v;
+        }
+        return size;
+    }
+
+    public:
+
+    string type,name;
+    vector<int> indexs;
+    int idscope,inscope,size;
+    bool leftvalue;
+
+    Var(){}
+    Var(const string &type,const string &name,const vector<int> &indexs){
+        this->type=type;this->name=name;
+        this->indexs=indexs;
+        idscope=0;inscope=-1;
+        leftvalue=1;
+        size=checkindex(indexs);
+    }
+    Var(const string &type,const string &name){
+        this->type=type;this->name=name;
+        idscope=0;inscope=-1;
+        leftvalue=1;
+        size=1;
+    }
+    inline int GetArrayPos(const vector<int> &indexs){
+        int pre=1,cnt=0,len=indexs.size();
+        for(int i=len-1;~i;i--){
+            cnt+=pre*indexs[i];
+            pre*=this->indexs[i];
+        }
+        return cnt;
+    }
+    inline int GetArrayPos(int *indexs,int len){
+        int pre=1,cnt=0;
+        for(int i=len-1;~i;i--){
+            cnt+=pre*indexs[i];
+            pre*=this->indexs[i];
+        }
+        return cnt;
+    }
+    inline void print(){
+        cout<<type<<" "<<name<<endl;
+        if(type=="array")for(int i=0;i<indexs.size();i++)cout<<name<<"["<<index[i]<<"] ";
+    }
+
+    
+
+};
+
